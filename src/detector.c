54c708097e4b646f7f13a0f9408687debfe5a734
Pandey <pandeyde@zermelo.cs.grinnell.edu>
2026-04-07 15:11:56 -0500

    before lock v1

--- src/detector.c
+++ src/detector.c
@@ -1,8 +1,25 @@
 #include "internal/detector.h"
 #include "internal/utils.h"
 
+static node_t* GLOBAL_LOCK_ORDERS = NULL;
 
 void before_lock(pthread_mutex_t *m) {
+    // get lock node if it exists
+    node_t* curr = GLOBAL_LOCK_ORDERS;
+    while (curr != NULL && curr->lock_number != m) {
+        curr = curr->next;
+    }
+
+    // insert new node if not found
+    if (curr == NULL) {
+        // TODO: re-think about free later
+        curr = malloc(sizeof(node_t*));
+        curr->next = GLOBAL_LOCK_ORDERS;
+        curr->lock_number = m;
+        curr->avoid_lock_numbers = malloc(sizeof(pthread_mutex_t*) * MAX_LOCK_COUNT);
+
+        GLOBAL_LOCK_ORDERS = curr;
+    }
     return;
 }
 
--- src/internal/detector.h
+++ src/internal/detector.h
@@ -1,6 +1,15 @@
 // Bookkeeping to detect deadlocks
 
 #include <pthread.h>
+#include <stdlib.h>
+#define MAX_LOCK_COUNT 100
+
+// global data structure to store state
+typedef struct node {
+    pthread_mutex_t* lock_number;
+    pthread_mutex_t** avoid_lock_numbers;
+    struct node* next;
+} node_t;
 
 void before_lock(pthread_mutex_t *m);
 
